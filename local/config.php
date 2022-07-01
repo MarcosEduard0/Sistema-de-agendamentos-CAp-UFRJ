f8b9eb9004d8c5a794ed1f1082e8d5ac6c1fd9c8
Marcos Eduardo <marcos.edaurdo22@gmail.com>
2022-06-30 23:36:44 -0300

    aplicando gitignore

---
+++ local/config.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+return array(
+	'config' => array(
+		'base_url' => 'http://192.168.0.105/sistema-de-agendamentos-cap-ufrj',
+		'log_threshold' => 1,
+		'index_page' => 'index.php',
+		'uri_protocol' => 'REQUEST_URI',
+	),
+
+	'database' => array(
+		'dsn' => 'mysql:host=localhost;dbname=agendamento',
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'agendamento',
+		'dbdriver' => 'pdo',
+	),
+
+);
